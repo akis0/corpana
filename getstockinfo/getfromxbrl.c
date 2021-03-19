bd84621f0dd60b95059bbd1438766156605a1746
Akihiro Isotani <67610636+akis0@users.noreply.github.com>
2021-03-20 00:59:15 +0900

    Update getfromxbrl.c

--- getstockinfo/getfromxbrl.c
+++ getstockinfo/getfromxbrl.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #define BUFLEN 2048
 #define SBUFLEN 512
@@ -11,20 +11,17 @@ FILE *fpoint;
 char buf[BUFLEN];
 void insidetag();
 int fileopen(char *filename);
-void clearbuf(char* b);
+void clearbuf(char *b);
 void insidetag();
-int  to_endofthetag(char * b);
+int to_endofthetag(char *b);
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    
+
     fileopen(argv[1]);
-    insidetag(); 
+    insidetag();
     return 0;
 }
-
-
 
 int fileopen(char *filename)
 {
@@ -38,85 +35,115 @@ int fileopen(char *filename)
         return 0;
     }
 }
-void clearbuf(char* b){
-    int i=0;
-    for(int i=0; i<sizeof(b)/sizeof(b[0]);i++){
-        b[i]='\0';
+void clearbuf(char *b)
+{
+    int i = 0;
+    for (int i = 0; i < sizeof(b) / sizeof(b[0]); i++)
+    {
+        b[i] = '\0';
     }
 }
-int to_endofthetag(char *b){
+int to_endofthetag(char *b)
+{
     clearbuf(b);
-    int i=0;
+    int i = 0;
     char c;
-    while ((c = getc(fpoint)) != EOF){
-        if (c == '>'){
+    while ((c = getc(fpoint)) != EOF)
+    {
+        if (c == '>')
+        {
             return 1;
-        }else{
-        b[i]=c;
-        i++;
         }
-        if(i==BUFLEN){
+        else
+        {
+            b[i] = c;
+            i++;
+        }
+        if (i == BUFLEN)
+        {
             printf("too long tag");
             return -1;
         }
     }
 }
 
-int judgewhichcontent(){
-    if((strstr(buf,"NameMajorShareholders"))!=NULL&&(strstr(buf,"MajorShareholdersMember")!=NULL)){
+int judgewhichcontent()
+{
+    if ((strstr(buf, "NameMajorShareholders")) != NULL && (strstr(buf, "MajorShareholdersMember") != NULL))
+    {
         return NAME;
-    }else if((strstr(buf,"AddressMajorShareholders"))!=NULL&&(strstr(buf,"MajorShareholdersMember")!=NULL)){
+    }
+    else if ((strstr(buf, "AddressMajorShareholders")) != NULL && (strstr(buf, "MajorShareholdersMember") != NULL))
+    {
         return ADDR;
-    }else if((strstr(buf,"NumberOfSharesHeld"))!=NULL&&(strstr(buf,"MajorShareholdersMember")!=NULL)){
+    }
+    else if ((strstr(buf, "NumberOfSharesHeld")) != NULL && (strstr(buf, "MajorShareholdersMember") != NULL))
+    {
         return NUMHOLD;
-    }else if((strstr(buf,"ShareholdingRatio"))!=NULL&&(strstr(buf,"MajorShareholdersMember")!=NULL)){
+    }
+    else if ((strstr(buf, "ShareholdingRatio")) != NULL && (strstr(buf, "MajorShareholdersMember") != NULL))
+    {
         return HOLDRATIO;
-    }else{
+    }
+    else
+    {
         return 0;
     }
 }
 
-void insidetag(){
+void insidetag()
+{
     char c;
     int f = 0;
-    int f0=0;
-    int m=0;
-    int x=0;
-    // char contents[5][12*SBUFLEN];
-    // int hindex[5];
-    // char cont[SBUFLEN];
-    while ((c = fgetc(fpoint)) != EOF){
-        int i=0;
-        int f=0;
-        if (c == '<'){
-            f=to_endofthetag(buf);
-        }else{
+    int f0 = 0;
+    int m = 0;
+    int x = 0;
+    char contents[5][12 * SBUFLEN];
+    for (int i = 0; i < 5; i++)
+    {
+        clearbuf(contents[i]);
+    }
+    int hindex[5];
+    while ((c = fgetc(fpoint)) != EOF)
+    {
+        int i = 0;
+        int f = 0;
+        if (c == '<')
+        {
+            f = to_endofthetag(buf);
+        }
+        else
+        {
             continue;
         }
-        if(f==1){
-            int ju=judgewhichcontent();
-            if(ju>0){
-                while((c=getc(fpoint))!=EOF){
-                    if(c!='<'){
-                        if(c!='\n'){
-                        //contents[ju][hindex[ju]++]=c;
-                        printf("%c",c);
+        if (f == 1)
+        {
+            int ju = judgewhichcontent();
+            if (ju > 0)
+            {
+                while ((c = getc(fpoint)) != EOF)
+                {
+                    if (c != '<')
+                    {
+                        if (c != '\n')
+                        {
+                            contents[ju][hindex[ju]++] = c;
+                            printf("%c", c);
                         }
-                    }else if(c=='<'){
+                    }
+                    else if (c == '<')
+                    {
                         to_endofthetag(buf);
-                        if(strstr(buf,"/jpcrp")!=NULL){
-                        // contents[ju][hindex[ju]++]='\n';
-                        printf("\n\n");
-                        break;
+                        if (strstr(buf, "/jpcrp") != NULL)
+                        {
+                            contents[ju][hindex[ju]++] = '\n';
+                            printf("%s","\n\n");
+                            break;
                         }
                     }
                 }
             }
         }
     }
-    // for(int i=0;i<4;i++){
-    //     for(int j=0;j<4;j++){
-    //         printf("%c",contents[i][j]);
-    //     }
-    // }
+    /*output*/
 }
